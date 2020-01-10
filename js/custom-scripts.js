7dd61c7699b028fc027c1fe373271b27b980524f
bobby00 <edwarobert6@gmail.com>
2020-01-10 21:09:40 +0300

    Added sign up view

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -15,13 +15,16 @@ $(document).ready(function(){
 
 document.getElementById("bck_sml_btn").style.display = "none";
 document.getElementById("sign_in_1").style.display = "none";
+document.getElementById("sign_up").style.display = "none";
 document.getElementById("swap_mini").style.display = "none";
+document.getElementById("swap_mini_2").style.display = "none";
 
 
 
 // document.getElementById("hideBottomNav").onclick = function() { 
 //     document.getElementById("bottomNav").style.display = "none";
 // }
+
 
 document.getElementById("choose_home_owner").onclick = function() { 
     x = document.getElementById("sign_in_1");
@@ -30,6 +33,21 @@ document.getElementById("choose_home_owner").onclick = function() {
     document.getElementById("bck_sml_btn").style.display = "block";
     document.getElementById("swap_large").style.display = "none";
     document.getElementById("swap_mini").style.display = "block";
+    document.getElementById("bottomNav").style.display = "block";
+    document.getElementById("swap_mini_2").style.display = "none";
+    document.getElementById("hidden_on_page_activity").style.display = "none";
+    document.getElementById("reduce_sze_active").style.width = "25%";
+} 
+
+document.getElementById("choose_helper").onclick = function() { 
+    x = document.getElementById("sign_in_1");
+    x.style.display = "block";
+    x.classList.add("fadeInUp");
+    document.getElementById("bck_sml_btn").style.display = "block";
+    document.getElementById("swap_large").style.display = "none";
+    document.getElementById("swap_mini").style.display = "none";
+    document.getElementById("bottomNav").style.display = "block";
+    document.getElementById("swap_mini_2").style.display = "block";
     document.getElementById("hidden_on_page_activity").style.display = "none";
     document.getElementById("reduce_sze_active").style.width = "25%";
 } 
@@ -40,6 +58,18 @@ document.getElementById("bck_sml_btn").onclick = function() {
     document.getElementById("hidden_on_page_activity").style.display = "block";
     document.getElementById("reduce_sze_active").style.width = "50%";
     document.getElementById("swap_mini").style.display = "none";
+    document.getElementById("swap_mini_2").style.display = "none";
     document.getElementById("swap_large").style.display = "block";
+    document.getElementById("sign_up").style.display = "none";
 }
+
+document.getElementById("sign_up_btn").onclick = function() { 
+    x = document.getElementById("sign_up");
+    x.style.display = "block";
+    x.classList.add("fadeInUp");
+
+    if($(x).css('display') == 'block') {
+      document.getElementById("bottomNav").style.display = "none";
+    }
+  }
 
